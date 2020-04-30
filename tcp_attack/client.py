0ef90cfd33093189d3400f63109f28e3e772df16
Nikita Koznev <nikitakoznev@gmail.com>
2020-04-30 23:07:42 +0300

    Add tcp to tcp attack

---
+++ tcp_attack/client.py
@@ -0,0 +1,24 @@
+import socket
+from threading import Thread
+
+import const_tcp
+
+
+class Client:
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((const_tcp.TCP_IP, const_tcp.TCP_PORT))
+
+    def senging(self):
+        while True:
+            self.sock.send(input().encode('UTF-8'))
+        self.sock.close()
+
+    def start(self):
+        send_thread = Thread(target=self.senging)
+        send_thread.start()
+
+
+if __name__ == '__main__':
+    new_client = Client()
+    new_client.start()
---
+++ tcp_attack/server.py
@@ -0,0 +1,42 @@
+import socket
+from threading import Thread
+
+import const_tcp
+
+
+class TCPServer:
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((const_tcp.TCP_IP, const_tcp.TCP_PORT))
+        self.sock.listen()
+
+    def listening_client(self, connection, address):
+
+        while True:
+            data = connection.recv(const_tcp.BUFFER_SIZE)
+            if not data:
+                break
+            print(f'\nSender: {address}\nMessage: {data.decode("UTF-8")}')
+
+        connection.close()
+        print(f'Stop connection: {address}')
+
+    def new_connection(self):
+        while True:
+            connection, address = self.sock.accept()
+
+            if connection:
+                new_client_thread = Thread(
+                    target=self.listening_client,
+                    args=(connection, address)
+                    )
+                new_client_thread.start()
+
+    def start(self):
+        parent_thread = Thread(target=self.new_connection)
+        parent_thread.start()
+
+
+if __name__ == '__main__':
+    server = TCPServer()
+    server.start()
